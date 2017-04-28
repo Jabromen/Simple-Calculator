e1558d4ca25bd04c4e24e4bfdd044fd44b6a264c
Jabromen <jab135@students.uwf.edu>
2017-04-28 14:43:37 -0500

    Initial Project Commit

---
+++ SimpleCalculator/Calculator.cs
@@ -0,0 +1,140 @@
+﻿/**
+ * A basic calculator class implementation.
+ * 
+ * @author - Jeffrey Bromen
+ * @date   - 4/28/2017
+ */
+
+using System;
+using System.Linq;
+using System.Text;
+
+namespace SimpleCalculator
+{
+    enum CalculatorOperations { NoOp, Add, Subtract, Multiply, Divide, Modulo };
+
+    class Calculator
+    {
+        private double Num1 { get; set; }
+        private double Num2 { get; set; }
+        private double Result { get; set; }
+
+        private StringBuilder sb = new StringBuilder();
+
+        private string display;
+        public string Display
+        {
+            get { return display; }
+            set
+            {
+                display = value != "" ? value : "0";
+            }
+        }
+
+        private CalculatorOperations Mode = CalculatorOperations.NoOp;
+
+        public Calculator() { Display = "0"; }
+
+        public void AppendDigit(string digit)
+        {
+            if (digit.All(Char.IsDigit))
+                sb.Append(digit);
+
+            Display = sb.ToString();
+        }
+
+        public void AppendDecimal()
+        {
+            if (!sb.ToString().Contains('.'))
+                sb.Append('.');
+
+            Display = sb.ToString();
+        }
+
+        public void ChangeSign()
+        {
+            if (sb.Length == 0)
+                return;
+
+            if (sb[0] != '-')
+                sb.Insert(0, '-');
+            else
+                sb.Remove(0, 1);
+
+            Display = sb.ToString();
+        }
+
+        public void ClearAll()
+        {
+            Num1 = 0;
+            Num2 = 0;
+            Result = 0;
+            Mode = CalculatorOperations.NoOp;
+            sb.Clear();
+            Display = sb.ToString();
+        }
+
+        public void Backspace()
+        {
+            if (sb.Length > 0)
+                sb.Remove(sb.Length - 1, 1);
+
+            Display = sb.ToString();
+        }
+
+        public void Operation(CalculatorOperations mode)
+        {
+            if (sb.Length == 0)
+            {
+                Mode = mode;
+                return;
+            }
+
+            Equals();
+
+            if (Mode == CalculatorOperations.NoOp && sb.Length > 0)
+            {
+                Num1 = Convert.ToDouble(sb.ToString());
+                sb.Clear();
+                Mode = mode;
+            }
+        }
+
+        public void Equals()
+        {
+            if (Mode == CalculatorOperations.NoOp || sb.Length == 0)
+                return;
+
+            Num2 = Convert.ToDouble(sb.ToString());
+            sb.Clear();
+
+            switch (Mode)
+            {
+                case CalculatorOperations.Add:
+                    Result = Num1 + Num2;
+                    break;
+
+                case CalculatorOperations.Subtract:
+                    Result = Num1 - Num2;
+                    break;
+
+                case CalculatorOperations.Multiply:
+                    Result = Num1 * Num2;
+                    break;
+
+                case CalculatorOperations.Divide:
+                    Result = Num1 / Num2;
+                    break;
+
+                case CalculatorOperations.Modulo:
+                    Result = Num1 % Num2;
+                    break;
+            };
+
+            Mode = CalculatorOperations.NoOp;
+            sb.Append(Result.ToString());
+
+            Display = Result.ToString();
+        }
+    }
+}
---
+++ SimpleCalculator/MainWindow.xaml.cs
@@ -0,0 +1,171 @@
+﻿/**
+ * GUI interaction for a simple calculator program.
+ * 
+ * @author - Jeffrey Bromen
+ * @date   - 4/28/2017
+ */
+
+using System.ComponentModel;
+using System.Windows;
+
+namespace SimpleCalculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private Calculator calc;
+
+        private string display;
+
+        public string Display
+        {
+            get { return display; }
+            set
+            {
+                display = value;
+                RaisePropertyChanged("Display");
+            }
+        }
+
+        private void RaisePropertyChanged(string propName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            calc = new Calculator();
+
+            this.DataContext = this;
+
+            Display = calc.Display;
+        }
+
+        private void Click_Button_CE(object sender, RoutedEventArgs e)
+        {
+            calc.ClearAll();
+            Display = calc.Display;
+        }
+
+        private void Click_Button_Back(object sender, RoutedEventArgs e)
+        {
+            calc.Backspace();
+            Display = calc.Display;
+        }
+
+        private void Click_Button_Mod(object sender, RoutedEventArgs e)
+        {
+            calc.Operation(CalculatorOperations.Modulo);
+            Display = calc.Display;
+        }
+
+        private void Click_Button_Plus(object sender, RoutedEventArgs e)
+        {
+            calc.Operation(CalculatorOperations.Add);
+            Display = calc.Display;
+        }
+
+        private void Click_Button_7(object sender, RoutedEventArgs e)
+        {
+            calc.AppendDigit("7");
+            Display = calc.Display;
+        }
+
+        private void Click_Button_8(object sender, RoutedEventArgs e)
+        {
+            calc.AppendDigit("8");
+            Display = calc.Display;
+        }
+
+        private void Click_Button_9(object sender, RoutedEventArgs e)
+        {
+            calc.AppendDigit("9");
+            Display = calc.Display;
+        }
+
+        private void Click_Button_Minus(object sender, RoutedEventArgs e)
+        {
+            calc.Operation(CalculatorOperations.Subtract);
+            Display = calc.Display;
+        }
+
+        private void Click_Button_4(object sender, RoutedEventArgs e)
+        {
+            calc.AppendDigit("4");
+            Display = calc.Display;
+        }
+
+        private void Click_Button_5(object sender, RoutedEventArgs e)
+        {
+            calc.AppendDigit("5");
+            Display = calc.Display;
+        }
+
+        private void Click_Button_6(object sender, RoutedEventArgs e)
+        {
+            calc.AppendDigit("6");
+            Display = calc.Display;
+        }
+
+        private void Click_Button_Mul(object sender, RoutedEventArgs e)
+        {
+            calc.Operation(CalculatorOperations.Multiply);
+            Display = calc.Display;
+        }
+
+        private void Click_Button_1(object sender, RoutedEventArgs e)
+        {
+            calc.AppendDigit("1");
+            Display = calc.Display;
+        }
+
+        private void Click_Button_2(object sender, RoutedEventArgs e)
+        {
+            calc.AppendDigit("2");
+            Display = calc.Display;
+        }
+
+        private void Click_Button_3(object sender, RoutedEventArgs e)
+        {
+            calc.AppendDigit("3");
+            Display = calc.Display;
+        }
+
+        private void Click_Button_Div(object sender, RoutedEventArgs e)
+        {
+            calc.Operation(CalculatorOperations.Divide);
+            Display = calc.Display;
+        }
+
+        private void Click_Button_Sign(object sender, RoutedEventArgs e)
+        {
+            calc.ChangeSign();
+            Display = calc.Display;
+        }
+
+        private void Click_Button_0(object sender, RoutedEventArgs e)
+        {
+            calc.AppendDigit("0");
+            Display = calc.Display;
+        }
+
+        private void Click_Button_Decimal(object sender, RoutedEventArgs e)
+        {
+            calc.AppendDecimal();
+            Display = calc.Display;
+        }
+
+        private void Click_Button_Equal(object sender, RoutedEventArgs e)
+        {
+            calc.Equals();
+            Display = calc.Display;
+        }
+    }
+}
